7fe3c8c38088752465d64b0f3413bde9602cc800
Martin Chibwe <mchibwe01@gmail.com>
2018-04-12 01:37:30 -0700

    setting up first project

---
+++ itunes songs/itunes songs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  itunes songs
+//
+//  Created by Martin Chibwe on 4/12/18.
+//  Copyright © 2018 Martin Chibwe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
